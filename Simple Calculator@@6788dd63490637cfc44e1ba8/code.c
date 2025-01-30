6f8d016eb5004a5a41eb2ec1a9b461f75d860f02
2410991578 <pranav1578.becse24@chitkara.edu.in>
2025-01-30 18:10:11 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,8 +2,8 @@
 int main(){
     int a,b;
     char luck;
-    scanf("%d" "%d",&a,&b);
-    scanf("%c",&luck);
+    scanf("%d" "%d\n",&a,&b);
+    scanf(" %c",&luck);
     if(luck=='+'){
         printf("%d",a+b);
     }
