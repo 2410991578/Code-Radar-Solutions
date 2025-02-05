0773d88bc93dd5a54fac2545743b665bfc44bf43
2410991578 <pranav1578.becse24@chitkara.edu.in>
2025-02-05 17:07:41 +0530

    Solution to Check Number Sign

--- Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
+++ Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
@@ -5,11 +5,11 @@ scanf("%d",&n);
 if(n>0){
     printf("Positive");
 }
-else if(n=0){
-    printf("Zero");
+else if(n<0){
+    printf("Negative");
 }
 else{
-    printf("Negative");
+    printf("Zero");
 }
 return 0;
 }
