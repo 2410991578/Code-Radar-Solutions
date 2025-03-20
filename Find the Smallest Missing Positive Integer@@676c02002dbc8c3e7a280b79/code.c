c0e072710a5423ee0c0ca6cddf8b6452057c69c6
Pranav Garg <pranavgarg771@gmail.com>
2025-03-20 22:40:39 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
+    
     int arr[n];
-    for(int i=0; i<n; i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    int sum=0;
-    for(int i=0; i<n; i++){
-        sum+=arr[i];
+    
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
     }
-    int r=(n*(n-1))/2;
-    printf("%d",sum-r);
+    
+    // Expected sum of first (n-1) natural numbers
+    int expected_sum = (n * (n - 1)) / 2;
+    
+    // Output the anomaly
+    printf("%d", sum - expected_sum);
     return 0;
 }
